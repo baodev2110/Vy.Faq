4e7035fb3523189702f57ea8f86999dcd34a4a59
Bao <bao91@protonmail.com>
2019-11-11 11:11:50 +0100

    Created models folder, dbcontext class and faq model class

---
+++ Vy.Faq/Models/FaqContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vy.Faq.Models
+{
+	public class FaqContext : DbContext
+	{
+		public FaqContext(DbContextOptions<FaqContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Faq> FaqItems { get; set; }
+	}
+}

09e9931c0a4e366c6ddf5516cf956ecc1bb3847c
Bao <s169969@oslomet.no>
2019-11-16 14:45:44 +0100

    Cleanup

---
+++ Vy.Faq/client-app/src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+      <a href="#">Spørsmål og svar</a>
+      <a href="#">Kontakt oss</a>
+      </nav>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          active: false
+      }
+  }
+}
+</script>
